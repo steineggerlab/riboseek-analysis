9b5d8518361a4e5474cfbc4a52fa76663bd0fdb5
pskvins <pskvins@snu.ac.kr>
2026-07-30 20:46:27 +0900

    Bug fixed in the AUC script

--- scripts/compute_auc.sh
+++ scripts/compute_auc.sh
@@ -5,7 +5,7 @@ target_tsv=$2
 lookup=$3
 out_roc=$4
 
-script="${BASH_SOURCE[0]}"
+script=$(dirname "$(realpath "$0")")
 
 gawk 'BEGIN {OFS="\t"} ARGIND==1 {a[$1]=$2;next;} ARGIND==2 {b[$1]=$2;next;} ARGIND==3 {print $0, b[$1], b[$2], a[$2]}' ${target_tsv} ${lookup} ${input_tsv} > ${input_tsv}.m8
 
